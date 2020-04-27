af4bb429f20d61b4d9dc244ae0a74461e826f876
Odiaz Bumma <odiazbumma@gmail.com>
2020-04-28 05:51:13 +0700

    Kickstart express js and connect to mysql

---
+++ app.js
@@ -0,0 +1,37 @@
+let express = require('express');
+let app = express();
+
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host    : 'localhost',
+    user    : 'root',
+    database: 'join_us',
+    password: 'develpass'
+});
+
+app.get("/", function(req, res) {
+    //res.send("Hello World!");
+    //console.log("Someone requested us!");
+
+    let q = "SELECT COUNT(*) AS count FROM users";
+    connection.query(q, function(error, results){
+        if (error) throw error;
+        //console.log(results[0].count);
+        res.send("Total number of users is " + results[0].count);
+    });
+});
+
+app.get("/joke", function(req, res) {
+    res.send("Hey I found you!");
+    console.log("Joke route requested");
+});
+
+app.get("/lucky_num", function(req, res) {
+    let num = Math.floor((Math.random() * 10) + 1);
+    res.send("Your lucky number is: " + num);
+});
+
+app.listen(8080, function(){
+    console.log("Server running on 8080!");
+});
